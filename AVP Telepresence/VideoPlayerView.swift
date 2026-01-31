5db6d4b8f16a5ad1d5e849670ea3ba43821792d1
JacobSalgado <153098612+JacobSalgado@users.noreply.github.com>
2026-01-31 17:36:10 -0500

    starting process of video player to work

---
+++ AVP Telepresence/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  AVP Telepresence
+//
+//  Created by Research on 1/31/26.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
